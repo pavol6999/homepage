fc4ac39b512e4becd0414b0e4efa04bfb86160f1
Pavol Krajkovič <pavol.krajkovic@gmail.com>
2024-11-13 22:14:34 +0100

    Add github workflow

--- next.config.mjs
+++ next.config.mjs
@@ -6,11 +6,15 @@ const withMDX = mdx({
     options: { },
 });
 
+
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+    reactStrictMode: true,
+    distDir: 'build',
+    output: 'export'
 };
 
 export default withNextIntl(withMDX(nextConfig));
