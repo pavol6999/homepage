a2f3731ebcc37f4a31f783a2d8fce8a45a88b0c8
Pavol Krajkovič <pavol.krajkovic@gmail.com>
2024-11-14 22:16:19 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,6 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
-    distDir: 'build',
 };
 
 export default withNextIntl(withMDX(nextConfig));
